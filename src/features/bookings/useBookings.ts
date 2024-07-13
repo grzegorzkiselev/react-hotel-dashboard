eb45e92c0a0a5999b9f84d7bb7ef512d3301febb
grzegorzkiselev <egorkakiselev@gmail.com>
2024-07-13 19:06:19 +0300

    382

--- src/features/bookings/useBookings.ts
+++ src/features/bookings/useBookings.ts
@@ -1,8 +1,10 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { useSearchParams } from "react-router-dom";
 import { getBookings } from "../../services/apiBookings";
+import { BOOKINGS_PER_PAGE } from "../../utils/constants";
 
 export const useBookings = () => {
+  const queryClient = useQueryClient();
   const [searchParams] = useSearchParams();
 
   const filterValue = searchParams.get("status");
@@ -29,5 +31,19 @@ export const useBookings = () => {
     queryFn: () => getBookings({ filter, sortBy, page }),
   });
 
+  const pageCount = Math.ceil(count / BOOKINGS_PER_PAGE);
+
+  if (page < pageCount) {
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page + 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page + 1 }),
+    });
+  }
+  if (page > 1) {
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page - 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page - 1 }),
+    });
+  }
   return { bookings, isLoading, error, count };
 };
